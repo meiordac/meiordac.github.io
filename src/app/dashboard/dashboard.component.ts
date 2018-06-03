a4a14ee3891ea9f910584a6c537f867635e2e092
Matias Iordache <matias.iordache@gmail.com>
2018-06-03 14:02:55 +0200

    add  banner and name

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,10 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  image = '/assets/person.png';
   cards = [
-    { title: 'Card 1', cols: 2, rows: 1 },
-    { title: 'Card 2', cols: 1, rows: 1 },
-    { title: 'Card 3', cols: 1, rows: 2 },
-    { title: 'Card 4', cols: 1, rows: 1 }
+    {
+      title: 'My Story',
+      cols: 2,
+      rows: 1,
+      content: `
+      I am currently living in Germany.
+      I studied a B.Sc. in Engineering at Pontificia Universidad Católica de Chile
+      which was ranked best university in Latin America according to QS Ranking (2014).
+      This program is recognized by ABET as being substantially equivalent to Computer Engineering.
+      Later I studied a M.Sc. in Engineering where I Graduated with honors from the same university.
+      I have experience working in different projects developing new software solutions.
+      I work mainly with .NET, JavaScript, SQL, Python, Ruby, etc.
+      I spend my free time travelling and studying German.`
+    }
   ];
 }
